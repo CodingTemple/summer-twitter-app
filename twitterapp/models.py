078d6ba2e582f5be15cdd03e6af41600b00f0c73
carter3689 <carter3689@gmail.com>
2019-07-23 12:05:07 -0500

    Updated code for login

--- twitterapp/models.py
+++ twitterapp/models.py
@@ -2,9 +2,18 @@ from flask_sqlalchemy import SQLAlchemy
 from twitterapp import app
 from werkzeug.security import generate_password_hash,check_password_hash
 
+
+#Flask-Login Imports
+from flask_login import UserMixin
+from twitterapp import login_manager
+
 db = SQLAlchemy(app)
 
-class User(db.Model):
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+class User(db.Model,UserMixin):
     id = db.Column(db.Integer, primary_key = True)
     username = db.Column(db.String(150),nullable = False)
     email = db.Column(db.String(150), unique = True, nullable = False)
